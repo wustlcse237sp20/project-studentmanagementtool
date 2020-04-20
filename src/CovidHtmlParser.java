973c6e9fe554765b3eaaa1c9e80e54bc51ad6788
ericsallinger <eric.sallinger303@gmail.com>
2020-04-19 22:38:21 -0500

    functional covid stats table

--- src/CovidHtmlParser.java
+++ src/CovidHtmlParser.java
@@ -9,61 +9,78 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
-public class covidHtmlParser {
+public class CovidHtmlParser {
 	
 	private static String totalCases;
 	private static String newCases;
 	private static String totalDeaths;
 	private static String newDeaths;
 	private static String recovered;
+	private static CovidStats usStats;
+
 	
 	public static void main(String args[]){
-		Document document;
+		
+		CovidStats usStats = new CovidStats();
 
-		try {
-			
-			document = Jsoup.connect("https://www.worldometers.info/coronavirus/country/us/").get();
-			getData(document);
 			print( "Total Cases " + totalCases);
 			print( "New Cases " + newCases);
 			print( "Total Deaths " + totalDeaths);
 			print( "New Deaths " + newDeaths);
 			print( "Recovered " + recovered);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
-	private static void getData(Document document) {
+	public CovidStats getData() {	
+		Document document = null;
+		try {
+			document = Jsoup.connect("https://www.worldometers.info/coronavirus/country/us/").get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		Elements mainStats = document.select(".maincounter-number"); //Get core stats
 		Elements fullStats = document.select("tbody > tr > td"); // get full stats
-		getTotalCases(mainStats);
-		getNewCases(fullStats);
-		getTotalDeaths(mainStats);
-		getNewDeaths(fullStats);
-		getRecovered(mainStats);
+		
+
+		
+		usStats = new CovidStats();
+		
+		usStats.setTotalCases(getTotalCases(mainStats));
+		usStats.setNewCases(getNewCases(fullStats));
+		usStats.setNewDeaths(getNewDeaths(fullStats));
+		usStats.setRecovered(getRecovered(mainStats));
+		usStats.setTotalDeaths(getTotalDeaths(mainStats));
+		return usStats;
+		
+		
 	}
 
-	private static void getRecovered(Elements mainStats) {
+	private static String getRecovered(Elements mainStats) {
 		recovered = mainStats.get(2).text();
+		return recovered;
 	}
 
-	private static void getNewDeaths(Elements fullStats) {
+	private static String getNewDeaths(Elements fullStats) {
 		newDeaths = fullStats.get(4).text();
+		return newDeaths;
 	}
 
-	private static void getTotalDeaths(Elements mainStats) {
+	private static String getTotalDeaths(Elements mainStats) {
 		totalDeaths = mainStats.get(1).text();
+		return totalDeaths;
 	}
 
-	private static void getNewCases(Elements fullStats) {
+	private static String getNewCases(Elements fullStats) {
 		newCases = fullStats.get(2).text();
+		return newCases;
 	}
 
-	private static void getTotalCases(Elements mainStats) {
+	private static String getTotalCases(Elements mainStats) {
 		totalCases = mainStats.get(0).text();
+		return totalCases;
 	}
 
 	public static void print(String string) {
